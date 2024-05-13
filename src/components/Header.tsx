3d2c32e9b07e973c3bf15265f2a06f2e938ed662
Oleksandr Sachuk <ovsachuk@gmail.com>
2024-05-13 17:01:24 +0300

    Eco smart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,68 @@
 import React from 'react';
-import {Box, Link} from '@mui/material';
+import {Grid, Link} from '@mui/material';
 
 export const Header = () => {
     return (
-        <header className="App-header">
-            <Box
+        <header>
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
                 sx={{
-                    typography: 'body1',
+                    typography: 'body-1',
                     '& > :not(style) ~ :not(style)': {
                         ml: 2,
                     },
+                    margin: '50px 0',
                 }}
             >
-                <Link href="#" color="#63638A">
+                <Link
+                    href="#"
+                    color="#63638A"
+                    sx={{
+                        textDecoration: 'none'
+                    }}
+                >
                     Home
                 </Link>
-                <Link href="#" color="#63638A">
+                <Link
+                    href="#"
+                    color="#63638A"
+                    sx={{
+                        textDecoration: 'none'
+                    }}
+                >
                     About us
                 </Link>
-                <Link href="#" color="#63638A">
+                <Link
+                    href="#"
+                    color="#63638A"
+                    sx={{
+                        textDecoration: 'none'
+                    }}
+                >
                     Services
                 </Link>
-                <Link href="#" color="#63638A">
+                <Link
+                    href="#"
+                    color="#63638A"
+                    sx={{
+                        textDecoration: 'none'
+                    }}
+                >
                     Testimonials
                 </Link>
-                <Link href="#" color="#63638A">
+                <Link
+                    href="#"
+                    color="#63638A"
+                    sx={{
+                        textDecoration: 'none'
+                    }}
+                >
                     Contact us
                 </Link>
-            </Box>
+            </Grid>
         </header>
     )
 }
